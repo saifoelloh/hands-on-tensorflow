0513b9fd24726288787a18aff2d4f0a00f772319
saifoelloh <saifoelloh@gmail.com>
2019-11-30 11:09:31 +0700

    first question

--- index.py
+++ index.py
@@ -20,6 +20,15 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 
+# # Trying to remove flatten and se the result
+# # ------------------------------------------
+# # Flatten our data into one layer from the initial form where is (nxn)
+# model = tf.keras.models.Sequential([
+#     tf.keras.layers.Flatten(),
+#     tf.keras.layers.Dense(128, activation=tf.nn.relu),
+#     tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+# ])
+
 model.compile(
     optimizer = 'adam',
     loss = 'sparse_categorical_crossentropy',
