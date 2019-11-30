87aada9526cfb98ba6641229e62628eb923487e1
saifoelloh <saifoelloh@gmail.com>
2019-11-30 11:10:05 +0700

    second question

--- index.py
+++ index.py
@@ -29,6 +29,15 @@ model = tf.keras.models.Sequential([
 #     tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 # ])
 
+# # Trying to changing number of last layer
+# # ------------------------------------------
+# # Last layer represent number of labels
+# model = tf.keras.models.Sequential([
+#     tf.keras.layers.Flatten(),
+#     tf.keras.layers.Dense(128, activation=tf.nn.relu),
+#     tf.keras.layers.Dense(5, activation=tf.nn.softmax)
+# ])
+
 model.compile(
     optimizer = 'adam',
     loss = 'sparse_categorical_crossentropy',
